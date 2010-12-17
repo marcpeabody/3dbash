d8367c9b37ddab3205fa9f4520edabe90588bddf
Leon Gersing <leon@edgecase.com>
2010-12-17 15:41:38 -0500

    adding boot strap file

--- jakefile.js
+++ jakefile.js
@@ -4,9 +4,9 @@ task('deploy',[], function(){
       exec  = require('child_process').exec,
       child;
 
-  child = exec('scp -r ./ root@184.73.180.169:/home/www/apps/3dbash',
+  console.log("uploading files to amazon");
+  child = exec('scp -r ./ root@3dbash.com:/home/www/apps/3dbash',
   function (error, stdout, stderr) {
-      console.log("uploading files to amazon");
       console.log('stdout: ' + stdout);
       if (error !== null) {
         console.log('exec error: ' + error);
@@ -14,3 +14,23 @@ task('deploy',[], function(){
       console.log("done.");
   });
 });
+
+desc("reboot server");
+task("reboot", [], function(){
+  var util = require('util'),
+      exec = require('child_process').exec,
+      child;
+
+  console.log("ssh to bounce it all.");
+  child = exec("ssh root@3dbash.com '/home/www/apps/3dbash/boot'",
+  function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+      console.log("done.");
+  });
+});
+
+desc("send and restart");
+task("send", ['deploy', 'reboot'], function(){console.log("done.");});
